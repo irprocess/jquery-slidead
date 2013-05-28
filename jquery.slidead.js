191d8c78a223383ae776f605294d4e0bb17fd6f1
Stefano Caronia <stefano.caronia@gmail.com>
2013-05-28 15:22:54 +0200

    Fix

--- jquery.slidead.js
+++ jquery.slidead.js
@@ -1,3 +1,44 @@
+/*
+	slideAd v0.5 beta 1
+
+	@author s.caronia (Mind S.r.l)
+	@dependencies
+		* jQuery Cookie Plugin v1.3.1
+		* https://github.com/carhartl/jquery-cookie
+
+
+	ES. UTILIZZO ------------------------------------------
+
+		// vedi defaults
+		$('#testbanner').slideAd({
+			autoClose:		10,
+			dismiss:		2,
+			autoOpen:		true,
+			openers:		'#apri',
+			onClose:		function(){
+				alert("Closed!");
+			}
+		});
+
+		------
+
+		<div id="testbanner">
+			TEST BANNER<br><br>
+			<a href="" class="slideAd-close">CLOSE</a> -
+			<a href="http://www.google.it" class="slideAd-dismiss" target="_blank">DONE</a>
+		</div>
+		<button id="apri">APRI</button>
+
+		------
+
+		$('#testbanner').slideAd('open');
+		$('#testbanner').slideAd('close');
+		$('#testbanner').slideAd('dismiss'); 	// imposta il cookie per non far riaprire l'ad e chiude
+		$('#testbanner').slideAd('reset'); 		// elimina il cookie
+
+*/
+
+
 (function($){
 	$.fn.slideAd = function(args) {
 
@@ -14,10 +55,25 @@
 			onClose:			function(){}			// funzione da eseguire su hide
 		};
 
+		// utilities
+		var utils = {
+			_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+			encode : function (input) {
+				var output = "";var i = 0;
+				var chr1, chr2, chr3, enc1, enc2, enc3, enc4;				
+				while (i < input.length) {
+					chr1 = input.charCodeAt(i++);chr2 = input.charCodeAt(i++);chr3 = input.charCodeAt(i++);enc1 = chr1 >> 2;enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);	enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);enc4 = chr3 & 63;
+					if (isNaN(chr2)) enc3 = enc4 = 64; else if (isNaN(chr3))enc4 = 64;
+					output = output +this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) + this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+				}
+				return output;
+			}
+		}
+		
 		var opts = {};
-
+		
 		var sad = this;
-
+		
 		// set css base
 		sad.css('display','none');
 		sad.css('position','relative');
@@ -38,7 +94,7 @@
 		}
 
 		// impostazioni cookie
-		var cookieName='_sad_'+sad.attr('id');
+		var cookieName='_sad'+utils.encode(window.location.pathname).substring(0,10).toLowerCase()+'_'+sad.attr('id');
 		var cookie=$.cookie?$.cookie(cookieName):'';
 		var cookieVal=(opts.dismiss>0?"sad.d#"+opts.dismiss:"sad.s#0");
 		
